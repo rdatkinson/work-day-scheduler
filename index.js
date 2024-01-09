78991c6f7b19f9a1d527674ef3022de8b35f9f7a
Rachel Atkinson <rachelatkinson@192.168.1.161>
2024-01-09 19:35:29 +0000

    added function to create business hour time blocks

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
     // Display the current day
     $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
+
+    // Function to create time blocks
+    function createTimeBlocks() {
+        const startHour = 9; // Start of business hours (9 AM)
+        const endHour = 17;  // End of business hours (5 PM)
